32a3bb616716f32d698d8900f590416b146a1842
Kris Cherven <krischerven@gmail.com>
2019-08-20 09:10:36 -0400

    removes unused file

--- src/main/java/com/grandcircus/SpringDemo/Data.java
+++
@@ -1,8 +0,0 @@
-package com.grandcircus.SpringDemo;
-import java.util.ArrayList;
-public class Data {
-	private static ArrayList<Account> accounts = new ArrayList<>();
-	public static void createAccount(String fname, String lname, String city, String address, String phone, String email) {
-		accounts.add(new Account(fname, lname, city, address, phone, email));
-	}
-}
