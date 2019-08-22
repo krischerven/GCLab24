d423e23b196dc6bdf4c49dc5fdfc9121c4ee9db3
Kris Cherven <krischerven@gmail.com>
2019-08-22 08:44:49 -0400

    consistency change

--- src/main/java/com/grandcircus/SpringDemo/Database.java
+++ src/main/java/com/grandcircus/SpringDemo/Database.java
@@ -14,33 +14,33 @@ public class Database {
 	final List<Account> accounts = new ArrayList<Account>();
 	// items
 	public List<Item> getAllItems() {
-		return template.query("select * from items;", new BeanPropertyRowMapper<Item>(Item.class));
+		return template.query("SELECT * FROM items;", new BeanPropertyRowMapper<Item>(Item.class));
 	}
 	public List<Item> getAllItems(String filter) {
-		final String query = "select * from items WHERE (item_name LIKE '%?%' OR description LIKE '%?%');".replace("?", filter);
+		final String query = "SELECT * FROM items WHERE (item_name LIKE '%?%' OR description LIKE '%?%');".replace("?", filter);
 		return template.query(query, new BeanPropertyRowMapper<Item>(Item.class));
 	}
 	public int addItem(Item item) {
-		final String query = "insert into items(item_name, description, quantity, price) values(?, ?, ?, ?);";
+		final String query = "INSERT INTO items(item_name, description, quantity, price) VALUES(?, ?, ?, ?);";
 		return template.update(query, item.getName(), item.getDescription(), item.getQuantity(), item.getPrice());
 	}
 	public int remItem(int id) {
-		final String query = "delete from items where id = ?;";
+		final String query = "DELETE FROM items WHERE id = ?;";
 		return template.update(query, id);
 	}
 	public boolean remOneItem(Item item) {
-		List<Item> items = template.query("select * from items where item_name = '" + item.getName() + "' ;", new BeanPropertyRowMapper<Item>(Item.class));
+		List<Item> items = template.query("SELECT * FROM items WHERE item_name = '" + item.getName() + "' ;", new BeanPropertyRowMapper<Item>(Item.class));
 		items.get(0).setQuantity(items.get(0).getQuantity()-1);
 		updateItem(items.get(0));
 		return true;
 	}
 	public int updateItem(Item item) {
-		final String query = "update items set item_name = ?, description = ?, quantity = ?, price = ? where item_name = ?;";
+		final String query = "UPDATE items SET item_name = ?, description = ?, quantity = ?, price = ? WHERE item_name = ?;";
 		return template.update(query, item.getName(), item.getDescription(), item.getQuantity(), item.getPrice(), item.getName());
 	}
 	// accounts
 	public int addAccount(Account acc) {
-		final String query = "insert into users(first_name, last_name, city, address, phone_num, email) values(?, ?, ?, ?, ?, ?);";
+		final String query = "INSERT INTO users(first_name, last_name, city, address, phone_num, email) VALUES(?, ?, ?, ?, ?, ?);";
 		return template.update(query, acc.getFirstName(), acc.getLastName(), acc.getCity(), acc.getAddress(), acc.getPhone(), acc.getEmail());
 	}
 }
